a5347d5c6d7bf79dd318f19a0a0b72c5b759e1d4
MuhammadYusuf0I <abduvasiyevmuhammadyusuf28@gmail.com>
2024-01-25 19:16:23 +0500

    homewor

---
+++ index.js
@@ -0,0 +1,129 @@
+//  1-masala;
+
+// 1-usul
+
+// function cube(number) {
+//   let number$ = Math.pow(number, 3);
+
+//   return number$;
+// }
+// console.log(cube(5));
+
+//   2-usul
+
+// const sum = function cube1(num) {
+//   let sum = Math.pow(num, 3);
+
+//   return sum;
+// };
+// console.log(sum(5));
+
+//3-usul
+
+// const sum_ = (num_) => {
+//   let sum_ = Math.pow(num_, 3);
+//   return sum_;
+// };
+// console.log(sum_(5));
+
+//     2-masala
+
+// 1-usul
+
+// function square(a, b) {
+//   let c;
+//   if (a > b) {
+//     c = a;
+//   } else {
+//     c = b;
+//   }
+//   return c * c;
+// }
+// console.log(square(2, 3));
+
+// 2-usul;
+
+// const Square = function square1(a, b) {
+//   let c;
+//   if (a > b) {
+//     c = a;
+//   } else {
+//     c = b;
+//   }
+
+//   return c * c;
+// };
+// console.log(Square(2, 1));
+
+//  3-usul;
+
+// const square_ = (a, b) => {
+//   let c;
+//   if (a > b) {
+//     c = a;
+//   } else {
+//     c = b;
+//   }
+//   return c * c;
+// };
+// console.log(square_(3, 4));
+
+//  3-masala
+
+// 1-usul;
+
+// function number_(num) {
+//   let one = num % 10;
+
+//   let ten = num % 100;
+
+//   let hundred = num % 1000;
+
+//   let tens = (ten - one) / 10;
+
+//   let hundreds = (hundred - on) / 100;
+
+//   let res = hundreds + tens + one;
+
+//   return res;
+// }
+// console.log(number_(333));
+
+// 2-usul;
+
+// const number_$ = function number_$(num) {
+//   let one_ = num % 10;
+
+//   let ten_ = num % 100;
+
+//   let hundred_ = num % 1000;
+
+//   let tens_ = (ten_ - one_) / 10;
+
+//   let hundreds_ = (hundred_ - ten_) / 100;
+
+//   let res = hundreds_ + tens_ + one_;
+
+//   return res;
+// };
+// console.log(number_$(555));
+
+
+//   3-usul;
+
+// const num_1 = (num) => {
+//   let one_1 = num % 10;
+
+//   let ten_1 = num % 100;
+
+//   let hundred_1 = num % 1000;
+
+//   let tens_1 = (ten_1 - one_1) / 10;
+
+//   let hundreds_1 = (hundred_1 - ten_1) / 100;
+
+//   let res = hundreds_1 + tens_1 + one_1;
+
+//   return res;
+// };
+// console.log(num_1(444));
